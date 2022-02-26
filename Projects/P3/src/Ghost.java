80dfefccea77f393a19310dd5291359fb3039048
chris <christopherjzhou@gmail.com>
2022-02-26 17:06:06 -0500

    method code

--- Projects/P3/src/Ghost.java
+++ Projects/P3/src/Ghost.java
@@ -20,7 +20,23 @@ public class Ghost{
 		return false;
 	}
 
-	public boolean is_pacman_in_range() { 
+	public boolean is_pacman_in_range() {
+		// checking x+- 1, y
+		for (int i = -1; i <= 1; i += 2) {
+			int x = myLoc.x + i;
+			int y = myLoc.y;
+			if (myMap.getLoc(new Location(x, y)).contains(Map.Type.PACMAN))
+				return true;
+		}
+
+		// checking x, y+- 1
+		for (int i = -1; i <= 1; i += 2) {
+			int x = myLoc.x;
+			int y = myLoc.y + i;
+			if (myMap.getLoc(new Location(x, y)).contains(Map.Type.PACMAN))
+				return true;
+		}
+		
 		return false;
 	}
 
