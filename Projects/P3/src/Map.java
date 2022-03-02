8357d2ea9b76aa0b8b50deb64da9647581559f09
JackFishburger <wenxuan1220@gmail.com>
2022-03-01 23:12:27 -0500

    Finished eatCookie but I don't think it is correct. This is really confusing

--- Projects/P3/src/Map.java
+++ Projects/P3/src/Map.java
@@ -94,8 +94,10 @@ public class Map{
 	}
 	
 	public JComponent eatCookie(String name) {
-		//update locations, components, field, and cookies
-		//the id for a cookie at (10, 1) is tok_x10_y1
-		return null;
+		if(components.get(name) instanceof CookieComponent){
+			return components.get(name);
+		} else {
+			return null;
+		}
 	}
 }
