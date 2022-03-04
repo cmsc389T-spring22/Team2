a111c28fb805b292714775dbbe00c95d6a8ed763
Jason Cavanaugh <jascav418@gmail.com>
2022-03-04 15:14:03 -0500

    Changed get_valid_moves code

--- Projects/P3/src/PacMan.java
+++ Projects/P3/src/PacMan.java
@@ -17,14 +17,15 @@ public class PacMan{
 	public ArrayList<Location> get_valid_moves() {
 		ArrayList<Location> output = new ArrayList<>();
 		output.add(new Location(this.myLoc.x + 1, this.myLoc.y));
-		output.add(new Location(this.myLoc.x + 1, this.myLoc.y + 1));
-		output.add(new Location(this.myLoc.x + 1, this.myLoc.y - 1));
 		output.add(new Location(this.myLoc.x - 1, this.myLoc.y));
 		output.add(new Location(this.myLoc.x, this.myLoc.y + 1));
 		output.add(new Location(this.myLoc.x, this.myLoc.y - 1));
-		output.add(new Location(this.myLoc.x - 1, this.myLoc.y + 1));
-		output.add(new Location(this.myLoc.x - 1, this.myLoc.y));
-		output.add(new Location(this.myLoc.x - 1, this.myLoc.y - 1));
+		for (int i = 0; i < output.size(); i++) {
+			if (myMap.getLoc(output.get(i)).contains(Map.Type.WALL) || myMap.getLoc(output.get(i)).contains(Map.Type.GHOST)) {
+                output.remove(i);
+                i--;
+            }
+		}
 		return output;
 	}
 
