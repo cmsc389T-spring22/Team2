e19c3068ab836fb22b6c9a4a1c53f2338632669f
Solomon Osa <mon@umd.edu>
2022-03-04 16:45:55 -0500

    missed importing Assert

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -1,14 +1,18 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import org.junit.Assert.*;
 
 public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
 		NoFrame frame = new NoFrame();
 		Ghost ghost = frame.addGhost(new Location(x, y), "name", Color.red); 
-
+		frame.initPlayers(); //Creates all of the players
+        //Start The Game
+        frame.startGame();
 
 		assertTrue(ghost.move() == true);
+
 	}
 }
