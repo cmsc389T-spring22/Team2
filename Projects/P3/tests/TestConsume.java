05e7236ad33b794b4c251a0c3537d7883c77f85a
Solomon Osa <mon@umd.edu>
2022-03-04 16:48:09 -0500

    missing import of Asset

--- Projects/P3/tests/TestConsume.java
+++ Projects/P3/tests/TestConsume.java
@@ -1,7 +1,7 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
-
+import static org.junit.Assert.*;
 import javax.swing.JComponent;
 
 public class TestConsume extends TestCase {
@@ -10,6 +10,10 @@ public class TestConsume extends TestCase {
 
 		NoFrame frame = new NoFrame();
 		PacMan pacman = frame.addPacMan(new Location(2, 2));
+		frame.initPlayers(); //Creates all of the players
+
+		//Start The Game
+		frame.startGame();
 		pacman.consume();
 
 		assertTrue(frame.getMap.getLoc(new Location(2, 2)).contains(Map.Type.COOKIE) == JComponent);
