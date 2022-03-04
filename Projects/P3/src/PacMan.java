b07bfbfd0e0f5356f3e32bd624b51326abd68fb0
Jason Cavanaugh <jascav418@gmail.com>
2022-03-04 15:17:05 -0500

    Changed move method for Pacman

--- Projects/P3/src/PacMan.java
+++ Projects/P3/src/PacMan.java
@@ -20,9 +20,13 @@ public class PacMan{
 
 	public boolean move() {
 		ArrayList<Location> moves = this.get_valid_moves();
+		int sz = moves.size();
+		int idx = (int) Math.floor(Math.random()*sz);
 		if (moves.size() > 0) {
-			this.myLoc = moves.get(0);
-			return true;
+			if (myMap.move("pacman", moves.get(idx), Map.Type.PACMAN)) {
+				this.myLoc = moves.get(idx);
+				return true;
+			}
 		}
 		return false;
 	}
