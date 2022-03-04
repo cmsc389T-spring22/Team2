792becac7ede8ab35eddc55169dedd61c992720a
Solomon Osa <mon@umd.edu>
2022-03-04 16:18:23 -0500

    consume test

--- Projects/P3/tests/TestConsume.java
+++ Projects/P3/tests/TestConsume.java
@@ -2,9 +2,17 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
+import javax.swing.JComponent;
+
 public class TestConsume extends TestCase {
 
 	public void testConsume() throws FileNotFoundException {
-		return null;	
+
+		NoFrame frame = new NoFrame();
+		PacMan pacman = frame.addPacMan(new Location(2, 2));
+		pacman.consume();
+
+		assertTrue(frame.getMap.getLoc(new Location(2, 2)).contains(Map.Type.COOKIE) == JComponent);
+
 	}
 }
