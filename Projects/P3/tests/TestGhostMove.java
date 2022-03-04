8c5d1b9bf42d5fd1f575f340789aee8a051bd584
Solomon Osa <mon@umd.edu>
2022-03-04 16:32:22 -0500

    Ghost move test

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -5,6 +5,10 @@ import java.io.*;
 public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
-	
+		NoFrame frame = new NoFrame();
+		Ghost ghost = frame.addGhost(new Location(x, y), "name", Color.red); 
+
+
+		assertTrue(ghost.move() == true);
 	}
 }
