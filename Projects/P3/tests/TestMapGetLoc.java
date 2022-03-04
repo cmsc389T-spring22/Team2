9975c084422b3a321e01b4435545d37f9cdaa651
JackFishburger <wenxuan1220@gmail.com>
2022-03-04 16:44:04 -0500

    finished getLoc test

--- Projects/P3/tests/TestMapGetLoc.java
+++ Projects/P3/tests/TestMapGetLoc.java
@@ -8,17 +8,17 @@ public class TestMapGetLoc extends TestCase {
 	
 	public void testMapGetLoc1() throws FileNotFoundException {
 		//Creating A Map
-		NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+		NoFrame frame1 = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 
 		Location loc1 = new Location(5,1);
 
 		//Creating Players
-		PacMan pacman = frame.addPacMan(loc1); //Creates PacMan at location x, y
+		PacMan pacman = frame1.addPacMan(loc1); //Creates PacMan at location x, y
 
 		HashSet<Map.Type> correctAns = new HashSet<Map.Type>();
 		correctAns.add(Map.Type.PACMAN);
 		correctAns.add(Map.Type.COOKIE);
 		
-		assertTrue(correctAns.equals(frame.getMap().getLoc(loc1)));
+		assertTrue(correctAns.equals(frame1.getMap().getLoc(loc1)));
 	}
 }
