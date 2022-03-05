f152b9b326872ac6d8d7760761bee46242334f5e
Christopher Zhou <44513611+christopherhzhou@users.noreply.github.com>
2022-03-04 22:00:36 -0500

    Fixed error with merging eat cookie changes

--- Projects/P3/src/Map.java
+++ Projects/P3/src/Map.java
@@ -124,14 +124,14 @@ public class Map {
 
 		return attacked;
     
-	public HashSet<Type> getLoc(Location loc) {
-		// wallSet and emptySet will help you write this method
-		return null;
-	}
-    
 	public JComponent eatCookie(String name) {
 		// update locations, components, field, and cookies
 		// the id for a cookie at (10, 1) is tok_x10_y1
-		return null;
+		if(components.get(name) instanceof CookieComponent){
+			this.cookies++;
+			return components.get(name);
+		} else {
+			return null;
+		}
 	}
 }
