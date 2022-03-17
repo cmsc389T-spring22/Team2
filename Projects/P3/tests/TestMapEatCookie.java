aae56119d29181d2b6204f84cb309d522eadf260
Jason Cavanaugh <jascav418@gmail.com>
2022-03-17 15:09:34 -0400

    Clean run of tests. Everything is working.

--- Projects/P3/tests/TestMapEatCookie.java
+++ Projects/P3/tests/TestMapEatCookie.java
@@ -1,10 +1,11 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import static org.junit.Assert.*;
 
 public class TestMapEatCookie extends TestCase{
 	
-	public void testMapEatCookie() {
+	public void testMapEatCookie() throws FileNotFoundException {
 		NoFrame frame1 = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 
 		Location loc1 = new Location(5,1);
--- Projects/P3/tests/TestMapGetLoc.java
+++ Projects/P3/tests/TestMapGetLoc.java
@@ -1,11 +1,13 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import java.util.HashSet;
+import static org.junit.Assert.*;
 
 
-public class TestMapGetLoc {
+public class TestMapGetLoc extends TestCase {
 	
-	public void testMapGetLoc() {
+	public void testMapGetLoc() throws FileNotFoundException {
 		//Creating A Map
 		NoFrame frame1 = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 
