f4201790a91f77d814e8677c6e06ec6c30c6947a
Christopher Zhou <44513611+christopherhzhou@users.noreply.github.com>
2022-03-17 14:08:30 -0400

    Added map-eatcookie test

--- Projects/P3/tests/TestMapEatCookie.java
+++ Projects/P3/tests/TestMapEatCookie.java
@@ -5,6 +5,13 @@ import java.io.*;
 public class TestMapEatCookie {
 	
 	public void testMapEatCookie() {
+		NoFrame frame1 = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 
+		Location loc1 = new Location(5,1);
+
+		//Creating Players
+		PacMan pacman = frame1.addPacMan(loc1);
+		
+		assertTrue(frame1.getMap().eatCookie("pacman") == null);
 	}
 }
